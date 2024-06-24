27cf548751c8c3cdb76f14684bf078e06ceb9e2b
syedasim-08 <syedasim@webllisto.com>
2024-06-24 12:08:28 +0530

    added art routing

---
+++ src/app/art/page.tsx
@@ -0,0 +1,11 @@
+import Typography from "@/design-systems/Atoms/Typography"
+
+
+
+const Art: React.FC = ()=>{
+    return (
+        <Typography className="text-center text-xxl text-black">
+            COMING SOON
+        </Typography>
+    )
+}
